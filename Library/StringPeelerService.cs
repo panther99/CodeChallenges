87c92dfd0a97d43aee856c69c9673fe87d704c2f
panther99 <nikola.stojakovic@hotmail.com>
2020-02-02 21:25:02 +0100

    Add StringPeelerService

---
+++ Library/StringPeelerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Library
+{
+    public class StringPeelerService
+    {
+        public string RemoveFirstAndLastLetter(string text)
+        {
+            string trimmedText = text.Trim();
+            return trimmedText.Length < 3
+                ? null
+                : trimmedText.Substring(1, trimmedText.Length - 2);
+        }
+    }
+}
